0b0252b245b98e5761ba5e623b3de86412782f21
dirkarnez <smalldirkalex@gmail.com>
2019-07-30 17:00:13 +0800

    - add multiplication_table.h

--- main.cpp
+++ main.cpp
@@ -1,14 +1,23 @@
-#include<iostream>
+#include <iostream>
+#include <string>
 #include "printlib.h"
 #include "tuplelib.h"
+#include "multiplication_table.h"
 
 using namespace std;
 using namespace tuplelib;
 using namespace printlib;
+using namespace multiplication_table;
 
 int main(int argc, char *argv[]) {
-	Tuple<int, char> t = makeTuple(10, 's');
-	print(get<0>(t), get<1>(t), 345, 45.6);
-	cin.get();
+	cout << Table<10>::value << endl;
+	//calc<1, 10, 1, 9>();
+	
+
+	//Tuple<int, char> t = makeTuple(10, 's');
+	//print(get<0>(t), get<1>(t), 345, 45.6);
+
+	system("pause");
+
 	return 0;
 }
